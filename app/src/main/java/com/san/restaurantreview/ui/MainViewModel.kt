32454d4d506bb18b6c38b8480d82cc272c78c30e
Ihsan Hadiansyach <91408822+hadiansyach@users.noreply.github.com>
2023-09-18 10:43:43 +0700

    implement viewmodel and refactoring postReview and findRestaurant (#1)

---
+++ app/src/main/java/com/san/restaurantreview/ui/MainViewModel.kt
@@ -0,0 +1,81 @@
+package com.san.restaurantreview.ui
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.san.restaurantreview.data.response.CustomerReviewsItem
+import com.san.restaurantreview.data.response.PostReviewResponse
+import com.san.restaurantreview.data.response.Restaurant
+import com.san.restaurantreview.data.response.RestaurantResponse
+import com.san.restaurantreview.data.retrofit.ApiConfig
+import retrofit2.Call
+import retrofit2.Response
+
+class MainViewModel : ViewModel() {
+    private val _restaurant = MutableLiveData<Restaurant>()
+    val restaurant: LiveData<Restaurant> = _restaurant
+
+    private val _listReview = MutableLiveData<List<CustomerReviewsItem>>()
+    val listReview: LiveData<List<CustomerReviewsItem>> = _listReview
+
+    private val _isLoading = MutableLiveData<Boolean>()
+    val isLoading: LiveData<Boolean> = _isLoading
+
+    companion object {
+        private const val TAG = "MainViewModel"
+        private const val RESTAURANT_ID = "uewq1zg2zlskfw1e867"
+    }
+
+    init {
+        findRestaurant()
+    }
+
+    private fun findRestaurant() {
+        _isLoading.value = true
+        val client = ApiConfig.getApiService().getRestaurant(RESTAURANT_ID)
+        client.enqueue(object : retrofit2.Callback<RestaurantResponse> {
+            override fun onResponse(
+                call: Call<RestaurantResponse>,
+                response: Response<RestaurantResponse>
+            ) {
+                _isLoading.value = false
+                if (response.isSuccessful) {
+                    _restaurant.value = response.body()?.restaurant
+                    _listReview.value = response.body()?.restaurant?.customerReviews
+                } else {
+                    Log.e(TAG, "onFailure: ${response.message()}")
+                }
+            }
+
+            override fun onFailure(call: Call<RestaurantResponse>, t: Throwable) {
+                _isLoading.value = false
+                Log.e(TAG, "onFailure: ${t.message.toString()}")
+            }
+        })
+    }
+
+    fun postReview(review: String) {
+        _isLoading.value = true
+        val client = ApiConfig.getApiService().postReview(RESTAURANT_ID, "Asep", review)
+        client.enqueue(object : retrofit2.Callback<PostReviewResponse> {
+            override fun onResponse(
+                call: Call<PostReviewResponse>,
+                response: Response<PostReviewResponse>
+            ) {
+                _isLoading.value = false
+                val responseBody = response.body()
+                if (response.isSuccessful) {
+                    _listReview.value = responseBody?.customerReviews
+                } else {
+                    Log.e(TAG, "onFailure: ${response.message()}")
+                }
+            }
+
+            override fun onFailure(call: Call<PostReviewResponse>, t: Throwable) {
+                _isLoading.value = false
+                Log.e(TAG, "onFailure: ${t.message}")
+            }
+        })
+    }
+}
